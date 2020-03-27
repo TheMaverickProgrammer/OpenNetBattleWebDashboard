de6428e60158582def06bc3ab9868e65162bf3e0
TheMaverickProgrammer <bmpeppers@gmail.com>
2020-03-27 17:04:15 -0500

    landing page added with signup form

--- open_net_battle_dashboard/src/App.vue
+++ open_net_battle_dashboard/src/App.vue
@@ -1,21 +1,28 @@
 <template>
   <div id="app">
     <div id="app-content">
-    <Navbar user="JamesKing"/>
-    <div> 
-      <router-view/>
-    </div>
+      <div v-if="isLoggedIn">
+        <Navbar user="JamesKing"/>
+        <div> 
+          <router-view/>
+        </div>
+      </div>
+      <div v-else>
+        <Landing/>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import Navbar from './components/Navbar';
+import Navbar from '@/components/Navbar';
+import Landing from '@/components/Landing';
 
 export default {
   name: 'App',
   components: {
     Navbar,
+    Landing
   }
 }
 </script>
---
+++ open_net_battle_dashboard/src/components/Landing.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <div v-if="show=='none'">
+            <b-button>Login</b-button>
+            <b-button @click="handleSignup">Signup</b-button>
+        </div>
+        <div v-else-if="show=='signup'">
+            <SignupForm @signup-complete="handleSignupCompleted"></SignupForm>
+        </div>
+    </div>
+</template>
+
+<script>
+import SignupForm from './SignupForm'
+
+export default {
+    data() {
+        return {
+            show: "none"
+        }
+    },
+    methods: {
+        handleSignup() {
+            this.show = "signup"
+        },
+        handleSignupCompleted() {
+            this.show = "none";
+        }
+    },
+    components: { 
+        SignupForm
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
